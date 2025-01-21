aee23a46214e7971e999a19168ce17a97a373d58
Abdul Aziz Shaikh <shaikhaziz9920@gmail.com>
2025-01-21 23:13:51 +0530

    appsa.py

--- appsa.py
+++ appsa.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import pandas as pd
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.mention import mention
-from streamlit_extras.badges import badge
-from streamlit_extras.metric_cards import style_metric_cards
 import random
 
 # Initialize VADER
